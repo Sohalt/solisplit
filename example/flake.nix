49ef44ee17e9e9b02a28aa1fe71f7978206396a7
sohalt <sohalt@sohalt.net>
2023-10-20 13:28:20 +0200

    wip

--- example/flake.nix
+++ example/flake.nix
@@ -1,6 +1,7 @@
 {
   description = "An example deployment of solisplit using caddy as a reverse proxy";
-  inputs.solisplit.url = "github:sohalt/solisplit";
+  #inputs.solisplit.url = "github:sohalt/solisplit"; # <- use this in your standalone flake
+  inputs.solisplit.url = ".."; # <- refering to the flake in parent directory
   outputs = {
     nixpkgs,
     solisplit,
@@ -9,9 +10,21 @@
     nixosConfigurations.example = nixpkgs.lib.nixosSystem {
       modules = [
         ({config, ...}: {
+          # --- genenric setup ---
+          # make a container config, so we don't need to think about bootloader, filesystems, etc
+          boot.isContainer = true;
+          nixpkgs.hostPlatform = "x86_64-linux";
+
+          # --- import flake ---
+          # make services.solisplit module available
           imports = [solisplit.nixosModules.solisplit];
+          # make solisplit package available
           nixpkgs.overlays = [solisplit.overlays.default];
+
+          # --- configure service ---
+          # enable solisplit
           services.solisplit.enable = true;
+          # configure reverse proxy
           services.caddy = {
             enable = true;
             virtualHosts = {
