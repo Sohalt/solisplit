4177f190ca62ce3c33309fe198fd95772f718dc6
sohalt <sohalt@sohalt.net>
2023-11-18 21:09:14 +0100

    server-address in nix module

--- module/solisplit.nix
+++ module/solisplit.nix
@@ -14,11 +14,17 @@ in
         default = 8000;
         description = "port to listen on";
       };
+      server-address = mkOption {
+        type = types.str;
+        description = "Full address (including scheme) where the service is running.";
+        example = "https://solisplit.example.com";
+      };
     };
     config = mkIf cfg.enable {
       systemd.services.solisplit = {
         wantedBy = ["multi-user.target"];
         script = "${pkgs.solisplit}/bin/solisplit --port ${toString cfg.port}";
+        environment.SERVER_ADDRESS = cfg.server_address;
       };
     };
   }
